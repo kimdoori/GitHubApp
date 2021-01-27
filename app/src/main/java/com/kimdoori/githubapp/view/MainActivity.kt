ff65843271630c4aa4f3af76788e593d5e154eb7
kimdoori <dudu@spoqa.com>
2021-01-27 21:09:40 +0900

    [Add] : MainViewModel binding

--- app/src/main/java/com/kimdoori/githubapp/view/MainActivity.kt
+++ app/src/main/java/com/kimdoori/githubapp/view/MainActivity.kt
@@ -1,5 +1,31 @@
 package com.kimdoori.githubapp.view
 
+import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.kimdoori.githubapp.R
+import com.kimdoori.githubapp.databinding.ActivityMainBinding
+import com.kimdoori.githubapp.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity()
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setupBinding()
+    }
+
+    private fun setupBinding() {
+        binding = DataBindingUtil
+            .setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+            .apply {
+                lifecycleOwner = this@MainActivity
+                viewModel = this@MainActivity.viewModel
+            }
+    }
+}
