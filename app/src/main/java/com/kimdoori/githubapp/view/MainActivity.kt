4b906bf48b10720e08469110c968c2cf261b8f41
kimdoori <dooit0228@gmail.com>
2021-02-07 23:11:19 +0900

    [Fix] : MainViewModelImpl 사용하도록

--- app/src/main/java/com/kimdoori/githubapp/view/MainActivity.kt
+++ app/src/main/java/com/kimdoori/githubapp/view/MainActivity.kt
@@ -10,13 +10,13 @@ import com.kimdoori.githubapp.R
 import com.kimdoori.githubapp.databinding.ActivityMainBinding
 import com.kimdoori.githubapp.domain.model.GitHubRepoModel
 import com.kimdoori.githubapp.viewcommon.BaseAdapter
-import com.kimdoori.githubapp.viewmodel.MainViewModel
+import com.kimdoori.githubapp.viewmodel.MainViewModelImpl
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModelImpl by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
