f4c19da82221cc256ced2d45e79f596e59e7d0f3
dudu <dooit0228@gmail.com>
2021-02-08 20:19:45 +0900

    [Review] : ktx 사용해서 visibility 적용
    Co-authored-by: Hyeonseok Kang <kekemusa37@gmail.com>

--- app/src/main/java/com/kimdoori/githubapp/binding_adapter/ViewBindingAdapter.kt
+++ app/src/main/java/com/kimdoori/githubapp/binding_adapter/ViewBindingAdapter.kt
@@ -1,13 +1,10 @@
 package com.kimdoori.githubapp.binding_adapter
 
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("bind_visibility")
 fun setVisibility(view: View, isVisible: Boolean) {
-    if (isVisible) {
-        view.visibility = View.VISIBLE
-    } else {
-        view.visibility = View.GONE
-    }
+    view.isVisible = isVisible
 }
