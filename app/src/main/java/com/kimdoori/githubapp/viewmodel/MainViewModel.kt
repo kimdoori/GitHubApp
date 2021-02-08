d9e50b974de9c7032548ea7827950173078c9c8c
kimdoori <dooit0228@gmail.com>
2021-02-08 20:49:04 +0900

    [Review] : MainViewModel을 interface로 변경

--- app/src/main/java/com/kimdoori/githubapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/kimdoori/githubapp/viewmodel/MainViewModel.kt
@@ -1,13 +1,12 @@
 package com.kimdoori.githubapp.viewmodel
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
 import com.kimdoori.githubapp.domain.model.GitHubRepoModel
 
-abstract class MainViewModel : ViewModel() {
+interface MainViewModel {
 
-    protected abstract val gitHubRepoList: LiveData<List<GitHubRepoModel>>
-    protected abstract val loading: LiveData<Boolean>
+    val gitHubRepoList: LiveData<List<GitHubRepoModel>>
+    val loading: LiveData<Boolean>
 
-    abstract fun fetchGitHubRepoList(userName: String)
+    fun fetchGitHubRepoList(userName: String)
 }
--- app/src/main/java/com/kimdoori/githubapp/viewmodel/MainViewModelImpl.kt
+++ app/src/main/java/com/kimdoori/githubapp/viewmodel/MainViewModelImpl.kt
@@ -2,13 +2,14 @@ package com.kimdoori.githubapp.viewmodel
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.kimdoori.githubapp.domain.model.GitHubRepoModel
 import com.kimdoori.githubapp.domain.usecase.GitHubRepoUseCase
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 
 class MainViewModelImpl @ViewModelInject constructor(
     private val gitHubRepoUseCase: GitHubRepoUseCase
-) : MainViewModel() {
+) : MainViewModel, ViewModel() {
 
     override val gitHubRepoList = MutableLiveData<List<GitHubRepoModel>>()
     override val loading = MutableLiveData<Boolean>()
