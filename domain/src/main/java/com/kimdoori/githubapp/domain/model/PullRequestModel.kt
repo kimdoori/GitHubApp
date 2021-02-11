b181e267ccf395d61f48a7fe100714a51704e5a3
kimdoori <dooit0228@gmail.com>
2021-02-11 20:07:27 +0900

    [Add] : PullRequestModel

---
+++ domain/src/main/java/com/kimdoori/githubapp/domain/model/PullRequestModel.kt
@@ -0,0 +1,31 @@
+package com.kimdoori.githubapp.domain.model
+
+import com.kimdoori.githubapp.data.entity.PullRequestResponse
+
+data class PullRequestModel(
+    val id: Int,
+    val title: String,
+    val body: String,
+    val state: String,
+    val url: String,
+    val htmlUrl: String,
+    val assignee: UserModel? = null,
+    val requestedReviewers: List<UserModel?>? = null,
+    val issueUrl: String? = null,
+) {
+    companion object {
+        fun mapFromEntity(pullRequestResponse: PullRequestResponse) = PullRequestModel(
+            id = pullRequestResponse.id,
+            title = pullRequestResponse.title,
+            body = pullRequestResponse.body,
+            state = pullRequestResponse.state,
+            url = pullRequestResponse.url,
+            htmlUrl = pullRequestResponse.htmlUrl,
+            assignee = pullRequestResponse.assignee?.let { UserModel.mapFromEntity(it) },
+            requestedReviewers = pullRequestResponse.requestedReviewers
+                ?.filterNotNull()
+                ?.map { UserModel.mapFromEntity(it) },
+            issueUrl = pullRequestResponse.issueUrl,
+        )
+    }
+}
