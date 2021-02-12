b5ca2ec00568b748137d2e2f9383954474061144
kimdoori <dooit0228@gmail.com>
2021-02-12 20:09:23 +0900

    [Remove] : 필요 없는 공백

--- app/src/main/java/com/kimdoori/githubapp/detail/RepoDetailViewModel.kt
+++ app/src/main/java/com/kimdoori/githubapp/detail/RepoDetailViewModel.kt
@@ -6,8 +6,8 @@ import com.kimdoori.githubapp.domain.model.PullRequestModel
 
 abstract class RepoDetailViewModel : ViewModel() {
 
-     abstract val pullRequestList: LiveData<List<PullRequestModel>>
-     abstract val loading: LiveData<Boolean>
+    abstract val pullRequestList: LiveData<List<PullRequestModel>>
+    abstract val loading: LiveData<Boolean>
 
     abstract fun fetchPullRequestList(userName: String, repoName: String)
 }
