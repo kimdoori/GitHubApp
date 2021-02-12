e3e3a608dd347e6aed68b353c08e0a9ace2a63aa
kimdoori <dooit0228@gmail.com>
2021-02-12 20:01:59 +0900

    [Fix] : public으로 변경
    - binding 이용할 수 있도록

--- app/src/main/java/com/kimdoori/githubapp/main/MainViewModel.kt
+++ app/src/main/java/com/kimdoori/githubapp/main/MainViewModel.kt
@@ -6,8 +6,8 @@ import com.kimdoori.githubapp.domain.model.GitHubRepoModel
 
 abstract class MainViewModel : ViewModel() {
 
-    protected abstract val gitHubRepoList: LiveData<List<GitHubRepoModel>>
-    protected abstract val loading: LiveData<Boolean>
+    abstract val gitHubRepoList: LiveData<List<GitHubRepoModel>>
+    abstract val loading: LiveData<Boolean>
 
     abstract fun fetchGitHubRepoList(userName: String)
 }
