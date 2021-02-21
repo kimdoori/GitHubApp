9e93f486ced03a551bf0382dc8b18a3ab70691e0
kimdoori <dooit0228@gmail.com>
2021-02-21 13:06:13 +0900

    [Impl] : RepoDetailActivity

---
+++ app/src/main/java/com/kimdoori/githubapp/pullrequest/PullRequestActivity.kt
@@ -0,0 +1,36 @@
+package com.kimdoori.githubapp.pullrequest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.kimdoori.githubapp.R
+import com.kimdoori.githubapp.databinding.ActivityPullrequestBinding
+
+class PullRequestActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPullrequestBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setupBinding()
+        setupWebView()
+    }
+
+    private fun setupBinding() {
+        binding = DataBindingUtil.setContentView<ActivityPullrequestBinding>(
+            this, R.layout.activity_pullrequest
+        ).apply {
+            lifecycleOwner = this@PullRequestActivity
+        }
+    }
+
+    private fun setupWebView() {
+        intent.getStringExtra(EXTRA_PULL_REQUEST_URL)?.let {
+            binding.pullRequestWebview.loadUrl(it)
+        }
+    }
+
+    companion object {
+        const val EXTRA_PULL_REQUEST_URL = "pullRequestUrl"
+    }
+}
